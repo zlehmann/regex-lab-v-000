70f78847bb9e1152249f70a1940608b54e5ca599
zlehmann@gmail.com <zlehmann@gmail.com>
2018-09-07 22:51:20 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,7 +7,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  if word.scan(/\b+un/).any? && word.scan(/\w+ing\b/).any? == true
+  if text.scan(/\b+un/).any? && text.scan(/\w+ing\b/).any? == true
     return true
   else
     return false
