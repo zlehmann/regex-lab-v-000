400f2f7c9ecf0eee587b3af9b1568cb8dcaed843
zlehmann@gmail.com <zlehmann@gmail.com>
2018-09-07 23:46:34 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,11 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.scan(/^[A-Z]/) && text.scan(/$*[^a-zA-Z]\s/) == true
+    return true
+  else
+    return false
+  end
 end
 
 def valid_phone_number?(phone)
