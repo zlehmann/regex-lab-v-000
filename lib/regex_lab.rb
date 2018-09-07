af84b87873a8f30b0730a59092550de02b9e48bb
zlehmann@gmail.com <zlehmann@gmail.com>
2018-09-07 23:13:39 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,11 +7,11 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  return text.match(/\bun\w+ing/)
+  return text.scan(/\bun\w+ing/)
 end
 
 def words_five_letters_long(text)
-  return text.match(/\w{5}/)
+  return text.scan(/\w{5}/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
