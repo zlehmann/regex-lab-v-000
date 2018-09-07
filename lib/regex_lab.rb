2cda8090b7056ad8e653e7406e03e135429652e8
zlehmann@gmail.com <zlehmann@gmail.com>
2018-09-07 23:47:24 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.scan(/^[A-Z]/) && text.scan(/$*[^a-zA-Z]\s/) == true
+  if text.scan(/^[A-Z]/) && text.scan(/\z*[^a-zA-Z]\s/) == true
     return true
   else
     return false
