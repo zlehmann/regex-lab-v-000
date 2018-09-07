7c5b35ac5188e1fdcb386ba883e18137876be62c
zlehmann@gmail.com <zlehmann@gmail.com>
2018-09-07 22:34:03 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,9 @@
 def starts_with_a_vowel?(word)
-
+  if word.scan(/[aeiou]/).any? == true
+    return true
+  else
+    return false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
