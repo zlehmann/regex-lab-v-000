6a0b2968f743eaa2add28b54530bf9d726501d97
zlehmann@gmail.com <zlehmann@gmail.com>
2018-09-07 23:14:41 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,7 +11,7 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-  return text.scan(/\w{5}/)
+  return text.scan(/^\w{5}\s/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
